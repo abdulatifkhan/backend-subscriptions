467482e60d773bccadb3cf01ef582699cb9cd317
abdulatifkhan <anvarkhanov96@gmail.com>
2021-03-01 18:54:26 +0500

    Add typeDefs

---
+++ typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+
+  union Response = Classified | Error
+
+  type Error {
+    code: Int
+    message: String!
+  }
+
+  type Classified {
+    id: ID!
+    title: String!
+  }
+
+  type Query {
+    classified: [Classified!]!
+  }
+
+  type Mutation {
+    createClassified(): Classified!
+  }
+
+  type Subscription {
+    createdClassified: Classified!
+  }
+`
+
+module.exports = typeDefs
